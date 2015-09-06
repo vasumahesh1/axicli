a9792dfdf9625ef2c9c41f792b562959b13ab5da
Vasu Mahesh <vasu.mahesh@hotmail.com>
2015-09-06 13:11:15 +0530

    Added - Support for SSH

--- index.js
+++ index.js
@@ -3,113 +3,37 @@
 var fs = require("fs");
 var colors = require('colors');
 
-var BashConfigPath = "/home/vasu/.bashrc";
-var ZshConfigPath = "/home/vasu/.zshrc";
+var BASH_CONFIG_PATH = "/home/vasu/.bashrc";
+var ZSH_CONFIG_PATH = "/home/vasu/.zshrc";
 
-var AxiConfigPath = "/home/vasu/.axirc";
-var ServerConfigPath = "/home/vasu/.axiserver";
-var CommandConfig = require('./package.json');
+var AXI_CONFIG_FILE = ".axirc";
+var AXI_CONFIG_PATH = "/home/vasu/" + AXI_CONFIG_FILE;
+
+var AXI_SHELL_CONFIG_FILE = ".axishrc";
+var AXI_SHELL_CONFIG = "/home/vasu/" + AXI_SHELL_CONFIG_FILE;
+
+var PACKAGE_CONFIG = require('./package.json');
 
 var serverConfig = "";
 
 
-var currentConfig = JSON.parse(fs.readFileSync(AxiConfigPath));
-currentConfig.user = currentConfig.user || "root";
+var currentConfig = JSON.parse(fs.readFileSync(AXI_CONFIG_PATH));
+currentConfig.user = "root";
 /**
  * Main Project Generator for Generating All Types of Projects in AxiCLI
  */
-// function ProjectGen() {
-// 	this.projectConfig = null;
-// 	fs.writeFileSync(AxiConfig, "");
-// };
-
-// ProjectGen.prototype.GenerateBaseConfig = function (projectType) {
-
-// 	var _generateIonicProject = function (isActonateSpecs) {
-// 		if (isActonateSpecs) {
-
-// 		}
-// 	};
-
-// 	var _generateActonateProject = function (existingConfig) {
-
-// 		existingConfig.fileSpecs = {};
-
-// 		existingConfig.fileSpecs.js = {};
-
-// 		existingConfig.fileSpecs.js.fileOrder = [];
-// 		existingConfig.fileSpecs.js.fileOrder.push("/**/*.config.js");
-// 		existingConfig.fileSpecs.js.fileOrder.push("/**/*.directive.js");
-// 		existingConfig.fileSpecs.js.fileOrder.push("/**/*.service.js");
-// 		existingConfig.fileSpecs.js.fileOrder.push("/**/*.ctrl.js");
-// 		existingConfig.fileSpecs.js.fileOrder.push("main.js");
-// 		existingConfig.fileSpecs.js.fileOrder.push("filters.js");
-// 		existingConfig.fileSpecs.js.fileOrder.push("services.js");
-// 		existingConfig.fileSpecs.js.fileOrder.push("directives.js");
-
-// 		return existingConfig;
-// 	};
-
-// 	var _generateDefaultProject = function () {
-// 		var baseConfig = {};
-
-// 		baseConfig.dependencies = {};
-// 		baseConfig.dependencies.js = [];
-// 		baseConfig.dependencies.css = [];
-
-// 		baseConfig.directories = {};
-// 		baseConfig.directories.developmentFolder = "dev";
-// 		baseConfig.directories.applicationFolder = "www/app";
-// 		baseConfig.directories.lessFolder = "dev/less";
-// 		baseConfig.directories.libFolder = "dev/lib";
-
-// 		return baseConfig;
-// 	};
-
-// 	switch (projectType) {
-// 	case "actonate":
-// 		this.projectConfig = _generateActonateProject(_generateDefaultProject());
-// 		break;
-
-// 	default:
-// 		Utils.logInfo("Custom Project - Building Config ...");
-// 		this.projectConfig = _generateDefaultProject();
-// 		break;
-// 	}
-// };
-
-// ProjectGen.prototype.InstallGruntTemplate = function () {
-// 	var _gruntData = fs.readFileSync(GruntTemplatePath);
-// 	fs.writeFileSync(GruntConfig, _gruntData);
-// };
-
-// ProjectGen.prototype.WriteAxiConfig = function () {
-// 	fs.writeFileSync(AxiConfig, JSON.stringify(this.projectConfig, null, '\t'));
-// };
-
-// ProjectGen.prototype.BuildConfig = function (projectType) {
-// 	this.GenerateBaseConfig(projectType);
-// 	this.InstallGruntTemplate();
-// 	this.WriteAxiConfig();
-// 	if (projectType == "custom") {
-// 		Utils.log("Please update .axirc file for custom directory structure for your project.");
-// 	}
-// };
 
 /**
  * Server Gen
  *
  * @class
  */
-
 var ServerGen = function () {};
 
 ServerGen.InstallServer = function (serverObject) {
 	var easySsh = function (serverObject) {
 		serverConfig += "alias ssh-" + serverObject.name + "='ssh " + currentConfig.user + "@" + serverObject.ip + "'\n";
 	};
-
-
 
 	easySsh(serverObject);
 };
@@ -130,14 +54,14 @@ Utils.prototype.logInfo = function (message) {
 	console.log("[AxiCLI] ".blue + message.blue);
 };
 
-Utils.prototype.updateConfig = function (configType, callback) {
+Utils.prototype.installConfig = function (configType, callback) {
 	if (configType == "server" && serverConfig && serverConfig != "") {
-		fs.exists(ServerConfigPath, function (exists) {
+		fs.exists(AXI_SHELL_CONFIG, function (exists) {
 			if (exists) {
-				fs.unlinkSync(ServerConfigPath);
+				fs.unlinkSync(AXI_SHELL_CONFIG);
 			}
 
-			fs.writeFileSync(ServerConfigPath, serverConfig);
+			fs.writeFileSync(AXI_SHELL_CONFIG, serverConfig);
 			callback(false, {
 				message: "Updated AxiServer Config"
 			});
@@ -149,41 +73,41 @@ Utils.prototype.updateConfig = function (configType, callback) {
 	}
 };
 
+var appendMissingConfigs = function (filePath) {
+	Utils.log("Reading file ... " + filePath);
+	var fileData = fs.readFileSync(filePath, "UTF-8");
+	for (var i = configCli.length - 1; i >= 0; i--) {
+		if (fileData.indexOf(configCli[i]) == -1) {
+			fs.appendFileSync(filePath, configCli[i] + "\n");
+		}
+	};
+};
+
 Utils.prototype.updateShellConfig = function (configTypes, callback) {
 	var configCli = [];
 
-	var appendMissingConfigs = function (filePath) {
-		Utils.log("Reading file ... " + filePath);
-		var fileData = fs.readFileSync(filePath);
-		for (var i = configCli.length - 1; i >= 0; i--) {
-			if (fileData.indexOf(configCli[i]) == -1) {
-				fs.appendFileSync(filePath, configCli[i] + "\n");
-			}
-		};
-	};
-
 	for (var i = configTypes.length - 1; i >= 0; i--) {
 		if (configTypes[i] == "server" && serverConfig && serverConfig != "") {
-			configCli.push(". ~/.axiserver");
+			configCli.push(". ~/" + AXI_SHELL_CONFIG_FILE);
 		}
 	}
 
-	fs.exists(ZshConfigPath, function (exists) {
+	fs.exists(ZSH_CONFIG_PATH, function (exists) {
 		if (exists) {
-			appendMissingConfigs(ZshConfigPath);
+			appendMissingConfigs(ZSH_CONFIG_PATH);
 			callback(false, {
 				message: "Updated ZSH Config"
 			});
 		} else {
-			fs.exists(BashConfigPath, function (exists) {
+			fs.exists(BASH_CONFIG_PATH, function (exists) {
 				if (exists) {
-					appendMissingConfigs(BashConfigPath);
+					appendMissingConfigs(BASH_CONFIG_PATH);
 					callback(false, {
 						message: "Updated Bash Config"
 					});
 				} else {
 					callback(true, {
-						message: "Can't Find Bash or ZSH Config File in ~"
+						message: "Can't Find Bash or ZSH Config File in ~ (home) directory"
 					});
 				}
 			});
@@ -197,7 +121,7 @@ var Utils = new Utils();
 var program = require('commander');
 
 program
-	.version(CommandConfig.version)
+	.version(PACKAGE_CONFIG.version)
 	.alias('axi')
 	.option('install <installType>', 'Install AxiCLI Components', /^(shell)$/i)
 	.option('update <updateType>', 'Update AxiCLI Components', /^(shell)$/i)
@@ -214,12 +138,6 @@ program
 // .option('dev', 'Development Watcher')
 // .option('prod', 'Production Watcher')
 
-// if (program.init) {
-// 	Utils.logInfo("Starting New Project ...");
-// 	var ProjectGen = new ProjectGen();
-// 	ProjectGen.BuildConfig(program.init);
-// }
-
 if (program.install) {
 	if (program.install === "shell") {
 		serverConfig = "";
@@ -228,7 +146,7 @@ if (program.install) {
 			ServerGen.InstallServer(currentConfig.servers[i]);
 		}
 
-		Utils.updateConfig("server", function (err, result) {
+		Utils.installConfig("server", function (err, result) {
 			if (err) {
 				Utils.logError(result.message);
 			} else {
